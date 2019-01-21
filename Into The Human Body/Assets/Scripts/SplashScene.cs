ef4a57250d6ea59e94f1a565e00fba0485acebbf
Mert Oyman <mertoyman@gmail.com>
2019-01-21 21:29:02 +0300

    Splash scene reorganized

---
+++ Into The Human Body/Assets/Scripts/SplashScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScene : MonoBehaviour
+{
+    public AudioSource _audio;
+
+	public void PlayAudio()
+	{
+		_audio.Play ();
+	}
+
+	void LoadNextScene()
+	{
+		SceneManager.LoadScene("OpeningScene");
+	}
+}
